6a05184c19d95b8cdb0f3480f8c6823609a1471c
kiritoenginer <rirocuingsis@gmail.com>
2017-03-15 15:21:19 -0400

    first commit

---
+++ client/upload.js
@@ -0,0 +1,44 @@
+Template.uploadForm.onCreated(function () {
+  this.currentUpload = new ReactiveVar(false);
+});
+
+Template.uploadForm.helpers({
+  currentUpload: function () {
+    return Template.instance().currentUpload.get();
+  }
+});
+Template.uploadedFiles.helpers({
+  uploadedFiles:function(){
+    console.log(IMAGES.find().fetch());
+    return IMAGES.find();
+  }
+});
+
+Template.uploadForm.events({
+  'change #fileInput': function (e, template) {
+    if (e.currentTarget.files && e.currentTarget.files[0]) {
+      // We upload only one file, in case
+      // multiple files were selected
+      var upload = IMAGES.insert({
+        file: e.currentTarget.files[0],
+        streams: 'dynamic',
+        chunkSize: 'dynamic'
+      }, false);
+
+      upload.on('start', function () {
+        template.currentUpload.set(this);
+      });
+
+      upload.on('end', function (error, fileObj) {
+        if (error) {
+          alert('Error during upload: ' + error);
+        } else {
+          alert('File "' + fileObj.name + '" successfully uploaded');
+        }
+        template.currentUpload.set(false);
+      });
+
+      upload.start();
+    }
+  }
+});
